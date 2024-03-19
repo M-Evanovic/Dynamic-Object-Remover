b6d237b454e832d3e1ab4f8ac36ff72a7a95e87a
M-Evanovic <72654195+M-Evanovic@users.noreply.github.com>
2024-03-19 16:51:44 +0800

    Update utility.cpp

--- src/utility.cpp
+++ src/utility.cpp
@@ -1,30 +1,64 @@
-#include "remover/utility.h"
+#pragma once
+#ifndef _REMOVER_H_
+#define _REMOVER_H_
 
-std::vector<double> split_line(std::string line, char delimiter) {
-    std::vector<double> output;
-    std::stringstream ss(line);
-    std::string temp;
+#include "remover/RosParam.h"
+#include "remover/patchworkpp.hpp"
 
-    while (getline(ss, temp, delimiter)) {
-        output.emplace_back(stof(temp));
-    }
-    return output;
-}
+class Remover : public RosParam
+{
+private:
+    std::vector<Eigen::Matrix4d> poses;
+    std::vector<pcl::PointCloud<PointType>::Ptr> scans;
+    std::vector<pcl::PointCloud<PointType>::Ptr> scans_without_ground;
+    std::vector<pcl::PointCloud<PointType>::Ptr> scans_ground;
 
-SphericalPoint Cartesian2Spherical(PointType point) {
-    SphericalPoint sphericalPoint {
-        std::atan2(point.y, point.x),
-        std::atan2(point.z, std::sqrt(point.x * point.x + point.y * point.y)),
-        std::sqrt(point.x * point.x + point.y * point.y + point.z * point.z)
-    };
-    return sphericalPoint;
-}
+    std::vector<int> dynamic_point_indices;
+    std::vector<int> static_point_indices;
 
-float radian2degree(float radian) {
-    return radian * 180.0 / M_PI;
-}
+    pcl::PointCloud<PointType>::Ptr global_map;
+    pcl::KdTreeFLANN<PointType>::Ptr kdtree_global_map;
 
-float degree2radian(float degree) {
-    return degree * M_PI / 180.0;
-}
+    pcl::PointCloud<PointType>::Ptr global_map_without_ground;
+    pcl::PointCloud<PointType>::Ptr global_map_ground;
 
+    pcl::PointCloud<PointType>::Ptr global_map_current;
+
+    pcl::PointCloud<PointType>::Ptr global_map_dynamic;
+    pcl::PointCloud<PointType>::Ptr ground_plane_points;
+    pcl::PointCloud<PointType>::Ptr global_map_static;
+
+    PatchWorkpp<PointType> groundSeperator;
+
+public:
+    Remover();
+    ~Remover();
+
+    void initialize_all(void);
+    void loadPosesFromMatrix(std::string txt);
+    void loadPosesFromQuaterniond(std::string txt);
+    void loadScans(std::string dir);
+
+    void octree_downsample(pcl::PointCloud<PointType>::Ptr& src, pcl::PointCloud<PointType>::Ptr& dst);
+    void generateGlobalMap(void);
+
+    cv::Mat scan2range(pcl::PointCloud<PointType>::Ptr scan, float resize_ratio);
+    std::pair<cv::Mat, cv::Mat> map2range(pcl::PointCloud<PointType>::Ptr scan, float resize_ratio);
+    void getSubMap(int index);
+    void transformGlobalMap2Local(int index);
+    std::vector<int> getDynamicPointIndexInEachScan(cv::Mat rangeImg_local, cv::Mat rangeImg_different, cv::Mat rangeImg_index);
+    void detectDynamicPoint(void);
+
+    void transformDynamicMap2Local(int index);
+    void revertStaticPoint(void);
+
+    void removePointUnderHeight(void);
+
+    void separateStaticPointIndexFromGlobalIndex(void);
+
+    void extractStaticMap(void);
+
+    void run(void);
+};
+
+#endif
